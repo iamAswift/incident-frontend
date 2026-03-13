63ca15ae9ddec810f787a7d10dc21f4dbd0bc45c
iamAswift <okohaustine460@gmail.com>
2026-03-13 12:03:29 +0100

    Add address search + nearby incident fixed radar 4

--- src/components/IncidentMap.jsx
+++ src/components/IncidentMap.jsx
@@ -16,8 +16,6 @@ import "leaflet/dist/leaflet.css";
 import "leaflet-control-geocoder";
 import "leaflet-control-geocoder/dist/Control.Geocoder.css";
 
-
-
 // Fix default marker icons
 delete L.Icon.Default.prototype._getIconUrl;
 
@@ -26,7 +24,6 @@ L.Icon.Default.mergeOptions({
   iconUrl: require("leaflet/dist/images/marker-icon.png"),
   shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
 });
-
 
 // Custom icons
 const icons = {
@@ -55,8 +52,6 @@ const icons = {
   }),
 };
 
-
-
 // Handle map click
 function MapClickHandler({ onMapClick }) {
   useMapEvents({
@@ -73,79 +68,57 @@ function MapClickHandler({ onMapClick }) {
   return null;
 }
 
-
-
 // Address search control
 function AddressSearch({ onLocationSelect }) {
   const map = useMap();
 
   React.useEffect(() => {
-
-
     const control = L.Control.geocoder({
       defaultMarkGeocode: false,
-      placeholder:  "Search for address...",
+      placeholder: "Search for address...",
       position: "topright",
     })
       .on("markgeocode", function (e) {
-
         const latlng = e.geocode.center;
-
         map.setView(latlng, 16);
 
         if (onLocationSelect) {
-          onLocationSelect({
-            lat: latlng.lat,
-            lng: latlng.lng,
-          });
+          onLocationSelect({ lat: latlng.lat, lng: latlng.lng });
         }
 
         L.marker(latlng).addTo(map);
-
       })
       .addTo(map);
 
     return () => map.removeControl(control);
-
   }, [map, onLocationSelect]);
 
   return null;
 }
-
-
 
 export default function IncidentMap({
   incidents = [],
   onMapClick,
   onLocationSelect
 }) {
-
   return (
-
     <div style={{ width: "100%", height: "500px", minHeight: "500px" }}>
-
       <MapContainer
         center={[9.082, 8.6753]}
         zoom={6}
         style={{ width: "100%", height: "100%" }}
         scrollWheelZoom
       >
-
         <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
 
         {/* Address Search */}
-        {onLocationSelect && (
-          <AddressSearch onLocationSelect={onLocationSelect} />
-        )}
+        {onLocationSelect && <AddressSearch onLocationSelect={onLocationSelect} />}
 
         {/* Map click */}
-        {onMapClick && (
-          <MapClickHandler onMapClick={onMapClick} />
-        )}
+        {onMapClick && <MapClickHandler onMapClick={onMapClick} />}
 
         {/* Incident markers */}
         {incidents.map((incident) => {
-
           if (!incident.lat || !incident.lng) return null;
 
           return (
@@ -154,19 +127,10 @@ export default function IncidentMap({
               position={[incident.lat, incident.lng]}
               icon={icons[incident.type] || new L.Icon.Default()}
             >
-
               <Popup>
-
                 <strong>{incident.title}</strong>
-
                 <p>{incident.description}</p>
-
-                {incident.date && (
-                  <p>
-                    Date: {new Date(incident.date).toLocaleString()}
-                  </p>
-                )}
-
+                {incident.date && <p>Date: {new Date(incident.date).toLocaleString()}</p>}
                 {incident.photo_url && (
                   <img
                     src={incident.photo_url}
@@ -175,16 +139,11 @@ export default function IncidentMap({
                     className="mt-2 max-w-full h-auto rounded"
                   />
                 )}
-
               </Popup>
-
             </Marker>
           );
         })}
-
       </MapContainer>
-
     </div>
-
   );
 }
