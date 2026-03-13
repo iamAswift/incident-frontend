99f97c1424be01bdc22eaf532395d86a89d97f63
iamAswift <okohaustine460@gmail.com>
2026-03-13 11:34:50 +0100

    Add address search + nearby incident fixed radar

--- src/components/IncidentMap.jsx
+++ src/components/IncidentMap.jsx
@@ -1,7 +1,87 @@
+// src/components/IncidentMap.jsx
+
+import React from "react";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMapEvents,
+  useMap
+} from "react-leaflet";
+
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+
+import "leaflet-control-geocoder";
+import "leaflet-control-geocoder/dist/Control.Geocoder.css";
+
+import * as LControlGeocoder from "leaflet-control-geocoder";
+
+
+// Fix default marker icons
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
+  iconUrl: require("leaflet/dist/images/marker-icon.png"),
+  shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
+});
+
+
+// Custom icons
+const icons = {
+  bandit: new L.Icon({
+    iconUrl: "/icons8-marker-b-50.png",
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+  }),
+
+  accident: new L.Icon({
+    iconUrl: "/icons8-car-accident-50.png",
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+  }),
+
+  robbery: new L.Icon({
+    iconUrl: "/icons8-robbery-80.png",
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+  }),
+
+  kidnapping: new L.Icon({
+    iconUrl: "/icons8-hostage-64.png",
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+  }),
+};
+
+
+
+// Handle map click
+function MapClickHandler({ onMapClick }) {
+  useMapEvents({
+    click(e) {
+      if (onMapClick) {
+        onMapClick({
+          lat: e.latlng.lat,
+          lng: e.latlng.lng,
+        });
+      }
+    },
+  });
+
+  return null;
+}
+
+
+
+// Address search control
 function AddressSearch({ onLocationSelect }) {
   const map = useMap();
 
   React.useEffect(() => {
+
     const geocoder = LControlGeocoder.geocoder();
 
     const control = LControlGeocoder.control({
@@ -22,11 +102,90 @@ function AddressSearch({ onLocationSelect }) {
         }
 
         L.marker(latlng).addTo(map);
+
       })
       .addTo(map);
 
     return () => map.removeControl(control);
-  }, [map]);
+
+  }, [map, onLocationSelect]);
 
   return null;
+}
+
+
+
+export default function IncidentMap({
+  incidents = [],
+  onMapClick,
+  onLocationSelect
+}) {
+
+  return (
+
+    <div style={{ width: "100%", height: "500px", minHeight: "500px" }}>
+
+      <MapContainer
+        center={[9.082, 8.6753]}
+        zoom={6}
+        style={{ width: "100%", height: "100%" }}
+        scrollWheelZoom
+      >
+
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+
+        {/* Address Search */}
+        {onLocationSelect && (
+          <AddressSearch onLocationSelect={onLocationSelect} />
+        )}
+
+        {/* Map click */}
+        {onMapClick && (
+          <MapClickHandler onMapClick={onMapClick} />
+        )}
+
+        {/* Incident markers */}
+        {incidents.map((incident) => {
+
+          if (!incident.lat || !incident.lng) return null;
+
+          return (
+            <Marker
+              key={incident.id}
+              position={[incident.lat, incident.lng]}
+              icon={icons[incident.type] || new L.Icon.Default()}
+            >
+
+              <Popup>
+
+                <strong>{incident.title}</strong>
+
+                <p>{incident.description}</p>
+
+                {incident.date && (
+                  <p>
+                    Date: {new Date(incident.date).toLocaleString()}
+                  </p>
+                )}
+
+                {incident.photo_url && (
+                  <img
+                    src={incident.photo_url}
+                    alt={incident.title}
+                    width={150}
+                    className="mt-2 max-w-full h-auto rounded"
+                  />
+                )}
+
+              </Popup>
+
+            </Marker>
+          );
+        })}
+
+      </MapContainer>
+
+    </div>
+
+  );
 }
